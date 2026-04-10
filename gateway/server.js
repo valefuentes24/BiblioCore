662d0edeaab4137599c1fe7a4e854ffbf03db69a
Ivy Lineth Guevara Delgado <ilguevarad@academia.usbbog.edu.co>
2026-04-09 20:51:05 -0500

    Update server.js

--- gateway/server.js
+++ gateway/server.js
@@ -104,6 +104,8 @@ app.get("/loans/user/:userId", async (req, res) => {
 // ===============================
 // LEVANTAR GATEWAY
 // ===============================
-app.listen(3000, () => {
-  console.log("API Gateway corriendo en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`API Gateway corriendo en http://0.0.0.0:${PORT}`);
 });
